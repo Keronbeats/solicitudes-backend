d511ee049d4913f46341fc0f48fe1c266ee9f3da
KeronBeats <byron.mancilla.rojas@gmail.com>
2022-12-04 22:26:54 -0300

    add more methods for api rest

--- src/routes/solicitudes.js
+++ src/routes/solicitudes.js
@@ -1,11 +1,20 @@
 import { Router } from 'express';
-import { getSolicitudes, createSolicitudes, updateSolicitudes, deleteSolicitudes, getSolicitud, update1Solicitudes} from '../controllers/solicitudes.controllers.js';
+import { getSolicitudes, 
+         createSolicitudes, 
+         updateSolicitudes, 
+         deleteSolicitudes, 
+         getSolicitud, 
+         update1Solicitudes,
+         uptadeCampoinSolicitud,
+         deleteSolicitudByid
+        } from '../controllers/solicitudes.controllers.js';
 const router = Router();
 
-router.get('/solicitudes', getSolicitudes);  
-router.get('/solicitudes/:id', getSolicitud);
+router.get('/solicitudes', getSolicitudes);   // OBTENER TODAS LAS SOLICITUDES DE LA TABLA
 
-router.post('/solicitudes', createSolicitudes);
+router.get('/solicitudes/:id', getSolicitud); // OBTENER SOLO 1 SOLICITUD POR ID
+
+router.post('/solicitudes', createSolicitudes);  //  PARA CREAR SOLICITUD
 
 router.delete('/solicitudes/:id', deleteSolicitudes);
 
@@ -13,5 +22,8 @@ router.put('/solicitudes/:tipo', updateSolicitudes);
 
 router.patch('/solicitudes/:id_encargado', update1Solicitudes);
 
+router.patch('/solicitudes/:id_solicitud', uptadeCampoinSolicitud); // PARA ACTUALIZAR CAMPO RESUELTO DE TABLA SOLICITUD CON EL ID DE SOLICITUD
+
+router.delete('/solicitudes/:id_solicitud', deleteSolicitudByid); // PARA ELIMINAR SOLICITUD POR ID DE LA TABLA SOLICITUD
 
 export default router;
